897df46147aa6ff1726f33cceb8c2b15800fdf27
youngrongoh <youngrong.oh@gmail.com>
2022-10-27 08:34:48 +0900

    1-1-1/실습1-7

---
+++ 1-algorithm/1-7.py
@@ -0,0 +1,12 @@
+print('1부터 n까지 정수의 합을 구합니다.')
+n = int(input('n값을 입력하세요.: '))
+
+sum = 0
+i = 0
+
+while i <= n:
+  sum += I
+  i += 1
+
+print(f'1부터 {n}까지 정수의 합은 {sum}입니다.')
+print(f'i값은 {i}입니다.')
