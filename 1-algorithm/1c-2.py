6ceafb89293da254a97247f76bf18be2d6638016
youngrongoh <youngrong.oh@gmail.com>
2022-10-26 09:12:51 +0900

    1-1-1/실습1c-2: 조건식 형태 변경

--- 1-algorithm/1c-2.py
+++ 1-algorithm/1c-2.py
@@ -1,19 +1,14 @@
 # 가독성이 떨어진다.
 # 오름차이든 내림차이든 크기순으로 정렬한 뒤에 인덱스로 가운데 값을 가져오는게 좋을 것 같다.
 def med(a, b, c):
-  if a >= b:
-    if b >= c:
-      return b
-    elif a <= c:
-      return a
-    else:
-      return c
-  elif a > c:
+  # (c <= a <= b) or (b <= a <= c)
+  if (b >= a and c <= a) or (b <= a and c >= a):
     return a
-  elif b > c:
-    return c
-  else:
+  # (c < b < a) or (a < b < c)
+  if (a > b and c < b) or (a < b and c > b):
     return b
+  # (a < c < b) or (b < c < a)
+  return c
 
 print('세 정수의 중앙값을 구합니다.')
 a = int(input('정수 a의 값을 입력하세요.: '))
