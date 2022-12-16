5cc2e3141d15d64703658cbefba803c546178af5
youngrongoh <youngrong.oh@gmail.com>
2022-12-16 09:20:56 +0900

    6-5/실습 6-9

---
+++ 6-sort/p6_9.py
@@ -0,0 +1,26 @@
+from typing import MutableSequence
+
+def shell_sort(a: MutableSequence) -> None:
+  n = len(a)
+  h = 1
+
+  while h < n // 9:
+    h = h * 3 + 1
+  
+  while h > 0:
+    for i in range(h, n):
+      j = i - h
+      tmp = a[i]
+      while j >= 0 and a[j] > tmp:
+        a[j + h] = a[j]
+        j -= h
+      a[j + h] = tmp
+    h //= 3
+
+if __name__ == '__main__':
+  print('셸 정렬을 수행합니다(h * 3 + 1의 수열을 사용).')
+  num = int(input('원소 수를 입력하세요.: '))
+  x = [None] * num
+
+  for i in range(num):
+    x[i] = int(input(f'x[{i}] = {x[i]}'))
